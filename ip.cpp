828da38add046561736a10bfd54600cb6eec4dbd
TheseCookiez <TheseCookiez@none.com>
2024-02-20 22:04:17 +0100

    init

--- ip.cpp
+++ ip.cpp
@@ -4,8 +4,19 @@
 #include <iostream>
 #include <vector>
 
+/*
+ * Source file name: "ipfile"
+ * Result file name: "result"
+ *
+ * Source file must be formatted as single IPv4 string pr. line
+ * Result file will be formatted as single IPv4 unsigned int pr. line
+ *
+ * if using .csv file, remove header first
+ */
+
+// Read ip source file into std::vector
 std::vector<std::string> get_file_content() {
-  std::fstream ipfile("ipfile2");
+  std::fstream ipfile("ipfile");
   std::vector<std::string> ip_vec;
   std::string line;
   if (ipfile.is_open()) {
@@ -17,6 +28,7 @@ std::vector<std::string> get_file_content() {
   return ip_vec;
 }
 
+// Save std:.vector to result file
 void save_file_content(std::vector<unsigned int> ip_int_vec) {
   std::ofstream result("result");
   for (const unsigned int &i : ip_int_vec) {
@@ -24,24 +36,28 @@ void save_file_content(std::vector<unsigned int> ip_int_vec) {
   }
 }
 
+// Convenience func to print std::vector containing strings
 void print_vec(std::vector<std::string> ip_vec) {
   for (const std::string &i : ip_vec) {
     std::cout << i << "\n";
   }
 }
 
+// Convenience func to print std::vector containing unsigned int
 void print_vec_int(std::vector<unsigned int> ip_vec) {
   for (const unsigned int &i : ip_vec) {
     std::cout << i << "\n";
   }
 }
 
+// Bitshift voodoo to convert IPv4 string to unsigned int
 unsigned int convertIPv4ToInt(const char *ip) {
   unsigned int a, b, c, d;
   sscanf(ip, "%u.%u.%u.%u", &a, &b, &c, &d);
   return (a << 24) | (b << 16) | (c << 8) | d;
 }
 
+// Loop the vector and convert each IPv4 to int repr. and push to new vec<int>
 std::vector<unsigned int> convert_to_int(std::vector<std::string> ip_vec) {
   std::vector<unsigned int> ip_int_vec;
   for (const std::string i : ip_vec) {
